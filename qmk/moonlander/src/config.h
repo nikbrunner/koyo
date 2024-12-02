29f36dab6b868744bddd904c7966e44ca0f993cb
nbr <nikolaus.brunner@protonmail.ch>
2024-12-02 22:07:18 +0100

    refactor(qmk): adjust tapping term
    - fix(moonlander/config): increase `TAPPING_TERM` from 150ms to 165ms
    - style(crkbd/config): remove redundant license header

--- qmk/moonlander/src/config.h
+++ qmk/moonlander/src/config.h
@@ -1,12 +1,8 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-
 #undef TAPPING_TERM
-#define TAPPING_TERM 150
+#define TAPPING_TERM 165
 
 #define COMBO_TERM 35
+
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #undef MOUSEKEY_WHEEL_DELAY
