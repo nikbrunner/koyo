d042e0829664e2a35b93d705276e82416ffd41f0
nbr <nikolaus.brunner@protonmail.ch>
2025-02-03 19:57:52 +0100

    feat(keymaps): resolve mod tabs on extended layer

--- qmk/crkbd/src/keymap.c
+++ qmk/crkbd/src/keymap.c
@@ -43,19 +43,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [LY_EXT] = LAYOUT_split_3x5_3(
-//    ┌──────────────────┬──────────────────┬──────────────────┬──────────────────┬─────────┐   ┌──────┬─────────┬───────────┬───────────┬─────────┐
-//    │     LGUI(q)      │     LGUI(w)      │     LGUI(e)      │     LGUI(r)      │ LGUI(t) │   │  no  │ LALT(u) │  LGUI(`)  │ LGUI(tab) │ HYPR(p) │
-//    ├──────────────────┼──────────────────┼──────────────────┼──────────────────┼─────────┤   ├──────┼─────────┼───────────┼───────────┼─────────┤
-//    │ TD(TD_EXT_GUI_A) │ TD(TD_EXT_GUI_S) │ TD(TD_EXT_GUI_D) │ TD(TD_EXT_GUI_F) │ LGUI(g) │   │ left │  down   │    up     │   rght    │ HYPR(;) │
-//    ├──────────────────┼──────────────────┼──────────────────┼──────────────────┼─────────┤   ├──────┼─────────┼───────────┼───────────┼─────────┤
-//    │     LGUI(z)      │     LGUI(x)      │     LGUI(c)      │     LGUI(v)      │ LGUI(b) │   │  no  │ CW_TOGG │ LSFT(tab) │    tab    │   no    │
-//    └──────────────────┴──────────────────┼──────────────────┼──────────────────┼─────────┤   ├──────┼─────────┼───────────┼───────────┴─────────┘
-//                                          │        no        │        no        │   no    │   │  no  │   no    │    no     │
-//                                          └──────────────────┴──────────────────┴─────────┘   └──────┴─────────┴───────────┘
-  LGUI(KC_Q)       , LGUI(KC_W)       , LGUI(KC_E)       , LGUI(KC_R)       , LGUI(KC_T) ,     KC_NO   , LALT(KC_U) , LGUI(KC_GRV) , LGUI(KC_TAB) , HYPR(KC_P)   ,
-  TD(TD_EXT_GUI_A) , TD(TD_EXT_GUI_S) , TD(TD_EXT_GUI_D) , TD(TD_EXT_GUI_F) , LGUI(KC_G) ,     KC_LEFT , KC_DOWN    , KC_UP        , KC_RGHT      , HYPR(KC_SCLN),
-  LGUI(KC_Z)       , LGUI(KC_X)       , LGUI(KC_C)       , LGUI(KC_V)       , LGUI(KC_B) ,     KC_NO   , CW_TOGG    , LSFT(KC_TAB) , KC_TAB       , KC_NO        ,
-                                        KC_NO            , KC_NO            , KC_NO      ,     KC_NO   , KC_NO      , KC_NO
+//    ┌──────┬──────┬──────┬──────┬────┐   ┌──────┬─────────┬───────────┬───────────┬─────────┐
+//    │  no  │  no  │  no  │  no  │ no │   │  no  │ LALT(u) │  LGUI(`)  │ LGUI(tab) │ HYPR(p) │
+//    ├──────┼──────┼──────┼──────┼────┤   ├──────┼─────────┼───────────┼───────────┼─────────┤
+//    │ lgui │ lalt │ lctl │ lsft │ no │   │ left │  down   │    up     │   rght    │ HYPR(;) │
+//    ├──────┼──────┼──────┼──────┼────┤   ├──────┼─────────┼───────────┼───────────┼─────────┤
+//    │  no  │  no  │  no  │  no  │ no │   │  no  │ CW_TOGG │ LSFT(tab) │    tab    │   no    │
+//    └──────┴──────┼──────┼──────┼────┤   ├──────┼─────────┼───────────┼───────────┴─────────┘
+//                  │  no  │  no  │ no │   │  no  │   no    │    no     │
+//                  └──────┴──────┴────┘   └──────┴─────────┴───────────┘
+  KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,     KC_NO   , LALT(KC_U) , LGUI(KC_GRV) , LGUI(KC_TAB) , HYPR(KC_P)   ,
+  KC_LGUI , KC_LALT , KC_LCTL , KC_LSFT , KC_NO ,     KC_LEFT , KC_DOWN    , KC_UP        , KC_RGHT      , HYPR(KC_SCLN),
+  KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO ,     KC_NO   , CW_TOGG    , LSFT(KC_TAB) , KC_TAB       , KC_NO        ,
+                      KC_NO   , KC_NO   , KC_NO ,     KC_NO   , KC_NO      , KC_NO
 ),
 
 [LY_SYM] = LAYOUT_split_3x5_3(
